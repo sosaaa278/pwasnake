34f69bd0bc090bd707b99d69a9fdf9e110656f21
sosaa278 <josesossa278@gmail.com>
2026-04-15 09:01:15 -0600

    prueba de pago real

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
 
